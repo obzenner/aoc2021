d817fa2af1081b402a617f55eb1c002eb61211e2
Dmitry Lambrianov <dmitry.lambrianov@nentgroup.com>
2021-12-08 05:32:23 +0100

    some styling

--- day7.js
+++ day7.js
@@ -46,13 +46,13 @@ const part1 = recursivelyFindBestFuel(input, 0, highest);
 const part2 = recursivelyFindBestFuel(input, 0, highest, true);
 
 const median = (arr) => {
-  const mid = Math.floor(arr.length / 2),
-    nums = [...arr].sort((a, b) => a - b);
+  const mid = Math.floor(arr.length / 2);
+  const nums = [...arr].sort((a, b) => a - b);
   return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
 };
 
 console.log({
   part1,
   part1WithMedian: calcFuelForPosition(input, median(input)),
-  part2,
+  part2
 });
